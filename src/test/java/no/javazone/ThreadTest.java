479382adeee50792b665543c846764f016a5dae2
Christin Gorman <christin.gorman@gmail.com>
2016-06-06 21:21:18 +0200

    only assert on processing time and present better output message

--- src/test/java/no/javazone/ThreadTest.java
+++ src/test/java/no/javazone/ThreadTest.java
@@ -3,23 +3,33 @@ package no.javazone;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class ThreadTest {
 
     @Test
     public void should_run_8_times_as_fast_with_8_cores() throws Exception {
-        long t = System.currentTimeMillis();
         int processors = Runtime.getRuntime().availableProcessors();
-        IntStream.range(0, processors * 100).forEach(i->Big.task());
-        long duration = System.currentTimeMillis() - t;
-        long mem = Runtime.getRuntime().totalMemory();
-        TaskRunner runner = new TaskRunner(processors * 100);
-        Supplier task = Big::task;
-        TaskRunner.Result result = runner.runTask(() -> new Thread(runner.track(task)).start());
+        int runs = processors * 100;
+        TaskRunner.Result singleThreadedResult = singleThreaded(runs);
+        TaskRunner.Result multiThreadedResult = multiThreaded(runs);
+        String msg = String.format("Expecting 8 times speedup, got only %d. Single Threaded time was %d",
+                singleThreadedResult.duration / multiThreadedResult.duration,
+                singleThreadedResult.duration);
 
-        Assert.assertTrue(String.format("Used %d compared to %d", result.memory, mem), result.memory > mem * processors/2);
-        Assert.assertTrue(String.format("Expecting 8 times speedup, got only %d. Single Threaded time was %d", duration / result.duration, duration), result.duration <= (duration / (processors-1)));
+        Assert.assertTrue(
+                msg,
+                multiThreadedResult.duration <= (singleThreadedResult.duration / (processors-1)));
+    }
+
+    private TaskRunner.Result multiThreaded(int runs) throws InterruptedException {
+        TaskRunner runner = new TaskRunner(runs);
+        return runner.runTask(() -> new Thread(runner.track(Big::task)).start());
+    }
+
+    private TaskRunner.Result singleThreaded(int runs) {
+        long t = System.currentTimeMillis();
+        IntStream.range(0, runs).forEach(i-> Big.task());
+        return new TaskRunner.Result(System.currentTimeMillis() - t, Runtime.getRuntime().totalMemory());
     }
 }
